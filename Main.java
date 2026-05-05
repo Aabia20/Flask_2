b623bb1af781ba0a67092f11655739aaaf0320ad
Aabia20 <247145@students.au.edu.pk>
2026-05-05 14:26:32 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String password = "12345";  // hardcoded password
+
+        int a = 10;
+        int b = 0;
+
+        System.out.println(a / b);  // divide by zero
+    }
+}
